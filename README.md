339af52f6ff7ec4041384924f747e4a3d003dc63
Yohanes Lopes <yohaneslopes@gmail.com>
2022-05-23 02:08:04 -0300

    Initial architecture

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # hardware-store-nodejs-prisma
+
 A hardware store API made in node.js with express and Prisma
+
+> Note: Create your own `.env` file, use `.env.example` as example.
---
+++ index.js
@@ -0,0 +1,9 @@
+const app = require("./server");
+
+const PORT = process.env.PORT || 8081;
+app.listen(PORT, () => {
+  console.log(`
+    Server is running on port ${PORT}.
+    http://localhost:${PORT}
+  `);
+});
